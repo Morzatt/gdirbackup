a40b58676b74c4a23f0e99d08998c3c0ebcc478b
Carlos <tineocarlos2606@gmail.com>
2024-09-24 23:00:46 -0400

    flags added

--- flags.go
+++ flags.go
@@ -1,14 +1,26 @@
 package main
 
 import (
+	"bufio"
+	"flag"
 	"fmt"
 	"os"
+	"path"
+	"strings"
 )
 
 var args = os.Args;
-var source string;
-var destination string;
+
+// Path to the directory to scan and print from.
+var source = flag.String("source", "", "path to the directory to scan and print from");
+// Path to the destination to print in.
+var destination = flag.String("destination", "", "path to the destination file to print in, if no destination is provided it will print out directly into the Standard Output")
+
+// Indicates the subdirectories inside the directory to scan to ignore
+var ignore = flag.String("ignore", "", "indicates the name of a subdirectory or file inside the directory to scan to ignore")
+
 var depth int = 0;
+var filler string;
 
 // Read reads the source path provided by the arguments, and prints its directory structure.
 func Read(source string) error {
@@ -16,15 +28,67 @@ func Read(source string) error {
 	if err != nil {
 		return err
 	}
+
+	if depth == 0 {
+		Write(fmt.Sprintf("%s \n", path.Base(source)), *destination)
+	}
+
 	for _, c := range content {		
+		if c.Name() == *ignore {
+			return nil
+		}
+
 		depth++
+		filler = strings.Repeat(fmt.Sprintf("|%*s", 3, ""), depth)
 		if c.IsDir() {
-			fmt.Printf("%*s%s/ \n", depth*3, "", c.Name())
+			Write(fmt.Sprintf("%s|─%s/ \n", filler, c.Name()), *destination)
 			Read(fmt.Sprintf("%s/%s", source, c.Name())); 
 		} else {
-			fmt.Printf("%*s|─%s\n", depth*3, "", c.Name())
+			Write(fmt.Sprintf("%s|─%s\n", filler, c.Name()), *destination)
 		}
 		depth--
 	}
+	return nil
+}
+
+func Write(content string, destination string) {
+	switch destination {
+		case "": 
+			if err := WriteToStdout(content); err != nil {
+				panic(err)
+			}
+		default: 
+			if err := WriteToFile(content, destination); err != nil {
+				panic(err)
+			}
+	}
+}
+
+func WriteToFile(content, destination string) error {
+	f, err := os.OpenFile(destination, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	w := bufio.NewWriter(f)
+	defer w.Flush()
+
+	_, err = w.WriteString(content)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func WriteToStdout(content string) error {
+	_, err := fmt.Print(content)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func ParseIgnoreFile() error {
 	return nil
 }
--- main.go
+++ main.go
@@ -1,19 +1,18 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 )
 
 func main() {
 	if len(args) < 2 {
-		fmt.Println("usage: gdirbackub [source_dirname] [destination] [options]")
+		fmt.Println("usage: gdirbackub [source_dirname] > [destination] [options]")
 		panic("not enough arguments provided")
 	}
 
-	source = args[1]
-	destination = args[0]
-
-	err := Read(source)
+	flag.Parse()
+	err := Read(*source)
 	if err != nil {
 		panic(err)
 	}
